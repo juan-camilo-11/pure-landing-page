7f1bf37328b86d938d9ffc6d0edb6f5a6fab1028
juan-camilo-11 <thekmilo84@gmail.com>
2024-05-21 07:06:01 -0500

    Subo version de Pure

--- src/modules/pages/versions/versions.tsx
+++ src/modules/pages/versions/versions.tsx
@@ -11,13 +11,13 @@ function Versions() {
                 {VersionsData.map((item) =>
                     <article>
                         <h4>{item.version}</h4>
-                        <p><b>Novedades generales:</b> {item.news === "" ? "N/A" : item.news}</p>
+                        <p><b>Novedades generales:</b> {item.news === "" ? "Sin novedad" : item.news}</p>
                         {item.components.map((component) =>
                             <ul>
                                 <li><b>Nombre:</b> {component.name}</li>
                                 <li><b>Etiqueta:</b> {component.Element}</li>
-                                <li><b>Bugs:</b> {component.bugs === "" ? "N/A" : component.bugs}</li>
-                                <li><b>Novedades:</b> {component.news === "" ? "N/A" : component.news}</li>
+                                <li><b>Bugs:</b> {component.bugs === "" ? "Sin novedad" : component.bugs}</li>
+                                <li><b>Novedades:</b> {component.news === "" ? "Sin novedad" : component.news}</li>
                             </ul>
                         )}
                     </article>
