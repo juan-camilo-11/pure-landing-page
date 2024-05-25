fa075ec42aa9d8a366d6732fc8da476a83fd0003
juan-camilo-11 <thekmilo84@gmail.com>
2024-05-25 16:50:43 -0500

    Se agrega version y se agrega los cambios

--- src/modules/components/input/input.tsx
+++ src/modules/components/input/input.tsx
@@ -3,7 +3,7 @@ import Detail from "../detail/detail";
 
 
 function Input() {
-    const styleVariables = ["--pure-input-focus"];
+    const styleVariables = ["--pure-input-focus","--color-input-message-text","--color-input-bg","--color-input-text"];
 
     return (
         <Detail styleVariables={styleVariables}>
